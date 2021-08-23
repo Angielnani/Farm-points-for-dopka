34824b617c8b464eea62a9b56d12b6d68a895375
Angielnani <80117994+Angielnani@users.noreply.github.com>
2021-08-23 16:38:30 +0300

    upload 1st task

--- t1.py
+++ t1.py
@@ -3,14 +3,21 @@ import json
 import random
 
 n = int(input("Enter a count of character: "))
-for i in range(n):
-    magic = random.randint(100, 1000)
-    resp = requests.get("https://www.anapioficeandfire.com/api/characters/" + str(magic))
-    data = resp.text
-    parse = json.loads(data)
-    name = parse["name"]
-    played = (parse["playedBy"])
-    actor = ''.join(played)
-    if not actor:
-        actor = '-'
-    print(name, actor)
+
+
+def get_random_character(n):
+
+    for i in range(n):
+        magic = random.randint(100, 1000)
+        resp = requests.get("https://www.anapioficeandfire.com/api/characters/" + str(magic))
+        data = resp.text
+        parse = json.loads(data)
+        name = parse["name"]
+        played = parse["playedBy"]
+        actor = ''.join(played)
+        if not actor:
+            actor = '-'
+        print(name, actor)
+
+
+get_random_character(n)
